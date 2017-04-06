601fc146d17a81cdc84b3cc6cac5093f5c564b26
gness1804 <graham.nessler@gmail.com>
2017-04-06 08:02:47 -0600

    Add working dropdown for tip percent

--- app/src/main/java/com/example/grahamnessler/tipcalculator/MainActivity.java
+++ app/src/main/java/com/example/grahamnessler/tipcalculator/MainActivity.java
@@ -2,12 +2,40 @@ package com.example.grahamnessler.tipcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    private Spinner spinner;
+    int tipPercent;
+    private String[] choices = {"10", "15", "20", "25"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createSpinner();
+    }
+
+    public void createSpinner () {
+        spinner = (Spinner) findViewById(R.id.tipPercentDropdown);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.choices_list, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        String choice = choices[pos];
+        tipPercent = Integer.parseInt(choice);
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
     }
 }
